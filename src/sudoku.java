45ca92cb638031098252852334519d10b5de87de
mahdzn13 <mahdzn13@gmail.com>
2017-06-18 12:09:16 +0200

    -Testing....

--- src/sudoku.java
+++ src/sudoku.java
@@ -1,49 +1,69 @@
-import java.util.Arrays;
+
+import java.util.Objects;
 import java.util.Random;
 
 public class sudoku {
     public static void main(String[] args) {
         sudokuMap sm = new sudokuMap();
-        String[][] sMap = sm.getMap();
-        System.out.println(sMap[0][0]);
-        System.out.println(Arrays.deepToString(sMap));
         sm.visualMap();
     }
 }
 
-class sudokuMap{
-
+class sudokuMap {
+    public static final String reset = "\u001B[0m";
+    public static final String red = "\u001B[31m";
     private int dimension = 9;
     private String[][] map = new String[dimension][dimension];
 
     //Genera un mapa de sudoku
-    sudokuMap(){
+    sudokuMap() {
         generateMap();
     }
 
     //Muestra el mapa del sudoku de una forma más humana
-    public void visualMap(){
+    public void visualMap() {
         System.out.println();
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < dimension; j++) {
                 System.out.print(map[i][j] + " ");
-                if ( j == 2 || j == 5 || j == 8)
-                System.out.print("  ");
+                if (j == 2 || j == 5 || j == 8)
+                    System.out.print("  ");
             }
             System.out.println();
-            if ( i == 2 || i == 5 || i == 8)
+            if (i == 2 || i == 5 || i == 8)
                 System.out.println();
         }
     }
-    public void generateMap(){
+
+    public void generateMap() {
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < dimension; j++) {
                 Random random = new Random();
-                map[i][j] = i + "-" + j;
+                String number = checkNumber(String.valueOf(random.nextInt(9) + 1), 0, 0);
+
+                map[i][j] = number;
                 //String.valueOf(random.nextInt(9) + 1);
                 //i + "-" + j
             }
         }
+    }
+
+    //wrong
+    private String checkNumber(String currentNumber, int posX, int posY) {
+        for (int l = 0; l < dimension; l++) {
+
+            for (int i = 0; i < dimension; i++) {
+                if (Objects.equals(map[l][i], currentNumber)) {
+                    map[l][i] = ">" + map[l][i];
+                }
+            }
+            for (int i = 0; i < dimension; i++) {
+                if (Objects.equals(map[i][l], currentNumber)) {
+                    map[i][l] = ">" + map[i][l];
+                }
+            }
+        }
+        return currentNumber;
     }
 
     public int getDimension() {
